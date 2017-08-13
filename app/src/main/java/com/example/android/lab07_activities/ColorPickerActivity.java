e5fd3535f50d7045ecf89cea440d76b8650a6ca1
koko <koko83447@gmail.com>
2017-08-14 00:14:59 +0800

    加入存檔

--- app/src/main/java/com/example/android/lab07_activities/ColorPickerActivity.java
+++ app/src/main/java/com/example/android/lab07_activities/ColorPickerActivity.java
@@ -2,11 +2,15 @@ package com.example.android.lab07_activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 
 public class ColorPickerActivity extends Activity {
+
+    private static final String TAG = "ColorPickerActivity";
 
     private int mColorInt;
     private CharSequence mColorName;
@@ -14,11 +18,58 @@ public class ColorPickerActivity extends Activity {
     public static final String BUNDLE_KEY_COLOR_INT = "com.example.android.colorInt";
     public static final String BUNDLE_KEY_COLOR_NAME = "com.example.android.colorName";
 
+    private void saveData(){
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(BUNDLE_KEY_COLOR_INT,mColorInt);
+        editor.putString(BUNDLE_KEY_COLOR_NAME,mColorName.toString());
+        editor.commit();
+        Log.d(TAG,"saveData() mColorInt = " + mColorInt + "mColorName = " + mColorName);
+    }
+
+    private void restoreData(){
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        mColorInt = prefs.getInt(BUNDLE_KEY_COLOR_INT,0);
+        mColorName = prefs.getString(BUNDLE_KEY_COLOR_NAME,"holo_red_light");
+        Log.d(TAG,"restoreData() mColorInt = " + mColorInt + "mColorName = " + mColorName);
+
+        RadioButton radio = null;
+        switch (mColorName.toString()){
+            case "holo_red_light":
+                radio = (RadioButton)findViewById(R.id.radio_holo_red_light);
+                break;
+            case "holo_orange_dark":
+                radio = (RadioButton)findViewById(R.id.radio_holo_orange_light);
+                break;
+            case "holo_green_light":
+                radio = (RadioButton)findViewById(R.id.holo_green_light);
+                break;
+            case "holo_blue_dark":
+                radio = (RadioButton)findViewById(R.id.holo_blue_light);
+                break;
+        }
+        if (radio != null){
+            radio.setChecked(true);
+        }
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume");
+        restoreData();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause");
+        saveData();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_picker);
-
+        Log.d(TAG,"onCreate");
         initColorData();
     }
 
@@ -26,6 +77,7 @@ public class ColorPickerActivity extends Activity {
         RadioButton radio = (RadioButton) findViewById(R.id.radio_holo_red_light);
         mColorInt = radio.getCurrentTextColor();
         mColorName = radio.getText();
+        Log.d(TAG,"clickColor() mColorInt = " + mColorInt + " mColorName = " +mColorName );
     }
 
     public void clickColor(View view) {
--- app/src/main/java/com/example/android/lab07_activities/MainActivity.java
+++ app/src/main/java/com/example/android/lab07_activities/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.android.lab07_activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     private static final int 取得選擇顏色 = 0;
     private static final int 取得選擇圖片 = 1;
@@ -15,12 +19,22 @@ public class MainActivity extends AppCompatActivity {
     private TextView m_tv_color;
     private ImageView m_iv_logo;
 
+    public static final String BUNDLE_KEY_COLOR_INT = "com.example.android.MainActivity.colorInt";
+    public static final String BUNDLE_KEY_COLOR_NAME = "com.example.android.MainActivity.colorName";
+    private int mColorInt;
+    private String mColorName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate()");
         init();
+        resotreData();
+        Log.d(TAG,"restoreData()");
     }
+
 
     private void init() {
         m_tv_color = (TextView)findViewById(R.id.tv_color);
@@ -32,15 +46,14 @@ public class MainActivity extends AppCompatActivity {
 
         startActivityForResult(intent, 取得選擇顏色);
     }
-    public void selectImage(View view){
-        Intent intent = new Intent(this, ImagePickerActivity.class);
 
-        startActivityForResult(intent, 取得選擇圖片);
-    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d(TAG,"onActivityResult()");
 
         if (resultCode != RESULT_OK){
             return;
@@ -49,18 +62,74 @@ public class MainActivity extends AppCompatActivity {
 
         switch (requestCode){
             case 取得選擇顏色:
-                int colorInt = bundle.getInt(ColorPickerActivity.BUNDLE_KEY_COLOR_INT);
-                CharSequence colorName = bundle.getCharSequence(ColorPickerActivity.BUNDLE_KEY_COLOR_NAME);
+                mColorInt = bundle.getInt(ColorPickerActivity.BUNDLE_KEY_COLOR_INT);
+                mColorName = bundle.getString(ColorPickerActivity.BUNDLE_KEY_COLOR_NAME);
 
-                m_tv_color.setText(colorName);
-                m_tv_color.setBackgroundColor(colorInt);
+                m_tv_color.setText(mColorName);
+                m_tv_color.setBackgroundColor(mColorInt);
                 break;
             case 取得選擇圖片:
                 int drawableId = bundle.getInt(ImagePickerActivity.KEY_IMAGE);
                 m_iv_logo.setImageResource(drawableId);
                 break;
         }
+    }
+    public void selectImage(View view){
+        Intent intent = new Intent(this, ImagePickerActivity.class);
 
+        startActivityForResult(intent, 取得選擇圖片);
+    }
+    private void saveData(){
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(MainActivity.BUNDLE_KEY_COLOR_INT,mColorInt);
+        editor.putString(MainActivity.BUNDLE_KEY_COLOR_NAME,mColorName);
+        editor.commit();
+    }
+
+    private void resotreData(){
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        mColorInt = prefs.getInt(MainActivity.BUNDLE_KEY_COLOR_INT,0);
+        mColorName = prefs.getString(MainActivity.BUNDLE_KEY_COLOR_NAME,null);
+        if (mColorName == null){
+            return;
+        }
+        m_tv_color.setBackgroundColor(mColorInt);
+        m_tv_color.setText(mColorName);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop()");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy()");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause()");
+        saveData();
+        Log.d(TAG,"saveData()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume()");
     }
 }
 
